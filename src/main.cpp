e2bdca5efcd3d857d573f12e38d467bf16fec8a6
Ella Truong <ellatruong95@gmail.com>
2026-05-25 16:27:34 -0700

    Phase 1: implement basic shell loop

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>    //provide pid_t = process(p_) ID(_id) type(_t), a datatype used for process IDs in Linux 
+#include <sys/wait.h>
+
+using namespace std;
+
+int main(){
+    string command;
+
+    while(true){
+        //show shell prompt
+        cout << "mini-shell> ";
+
+        //read user input
+        getline(cin, command);
+
+        //exit shell if user user types "exit"
+        if (command == "exit") {
+            break;
+        }
+
+        //if not, create a child process
+        pid_t id = fork();    //fork() return process ID type
+
+        if(id==0){
+            //child process executes command
+            execlp(command.c_str(), command.c_str(), NULL);
+
+            //runs only if conmmand fails
+            cout << "Command not found" << endl;
+            exit(1);
+
+        }else{
+            //present waits for child
+            wait(NULL);
+        }
+    }
+
+    return 0;
+}
